0c8f45fdda4792d4fb76ad07d679932b93a4475d
mosesbenjamin <mavewreek@gmail.com>
2021-06-04 22:39:56 +0100

    Navbar and layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
-    <Nuxt />
+    <div id="navbar">
+      <div class="navbar-links">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/videos">Videos</nuxt-link>
+        <nuxt-link to="/theatre">Theatre</nuxt-link>
+      </div>
+    </div>
+    <div id="default-body">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <style>
+#navbar {
+  height: 40px;
+  width: 100%;
+  background-color: #333;
+}
+
+#navbar .navbar-links {
+  max-width: 800px;
+  margin: auto
+}
+
+#navbar a {
+  color: #fff;
+  text-decoration: none;
+  margin-right: 10px;
+}
+
+#default-body {
+  max-width: 800px;
+  margin: auto
+}
 html {
   font-family:
     'Source Sans Pro',
---
+++ layouts/theatre.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div id="theatre-body">
+          <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #theatre-body {
+    background-color: black;
+    height: 100vh;
+    color: #fff;
+  }
+</style>
