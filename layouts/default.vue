345da877faefcf1aa56fcbc290ea249e6410a461
mosesbenjamin <mavewreek@gmail.com>
2021-06-05 23:25:14 +0100

    Veutify navbar, build homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,16 @@
 <template>
-  <div>
-    <div id="navbar">
-      <div class="navbar-links">
-        <nuxt-link to="/">Home</nuxt-link>
-        <nuxt-link to="/videos">Videos</nuxt-link>
-        <nuxt-link to="/theatre">Theatre</nuxt-link>
-      </div>
-    </div>
-    <div id="default-body">
+  <v-app>
+    <v-app-bar app color="green">
+      <v-btn text to="/">Vue Screencasts</v-btn>
+      <v-btn text to="/videos">Videos</v-btn>
+    </v-app-bar>
+    <v-content>
       <Nuxt />
-    </div>
-  </div>
+    </v-content>
+  </v-app>
 </template>
 
 <style>
-#navbar {
-  height: 40px;
-  width: 100%;
-  background-color: #333;
-}
-
-#navbar .navbar-links {
-  max-width: 800px;
-  margin: auto
-}
-
-#navbar a {
-  color: #fff;
-  text-decoration: none;
-  margin-right: 10px;
-}
 
 #default-body {
   max-width: 800px;
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios"
+    "@nuxtjs/axios",
+    "@nuxtjs/vuetify"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
